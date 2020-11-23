f8f031e940f6fbc930c81d4f966731f7481d9f9c
tyandrewgood <50933779+tyandrewgood@users.noreply.github.com>
2020-11-23 09:24:40 -0500

    Add files via upload

---
+++ config/config.default.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var config = require('./config.webgme'),
+    validateConfig = require('webgme/config/validator');
+
+// Add/overwrite any additional settings here
+// config.server.port = 8080;
+// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+
+config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
+config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
+config.requirejsPaths['jointjscss'] = './node_modules/jointjs/dst/joint';
+
+
+
+config.plugin.allowServerExecution = true;
+
+validateConfig(config);
+module.exports = config;
